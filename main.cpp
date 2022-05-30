df2f058f9553ecec7023f14f5b9e91a963d19000
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2022-05-30 19:26:48 +0300

    project file configuration converted to gui application configuration

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-#include <QCoreApplication>
+#include <QApplication>
 #include <QDebug>
+#include "Gui/MainWindow.h"
 #include "Backend/FileStorageSubSystem/FileStorageManager.h"
 #include "Backend/FileMonitorSubSystem/FileMonitoringManager.h"
 #include "Backend/FileMonitorSubSystem/FileMonitoringManagerIntegrationTest.h"
 #include "Backend/FileMonitorSubSystem/MonitoredDirDb.h"
-#include <QFile>
-
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
+    QApplication app(argc, argv);
     FileStorageManager storageManager(app.applicationDirPath() + "/backup", "C:/Users/AppData/Local");
 
     QStringList predictionList;
@@ -17,6 +16,9 @@ int main(int argc, char *argv[])
     predictionList << "not_exist";
 
     FileMonitoringManagerIntegrationTest fmmITest(predictionList);
+
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
