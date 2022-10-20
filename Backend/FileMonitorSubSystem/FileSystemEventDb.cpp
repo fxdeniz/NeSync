e9deee227a154cfc295562fb4e0ad83f77aa637c
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2022-10-20 21:47:16 +0300

    timestamp column added to `FileSystemEventDb`

--- Backend/FileMonitorSubSystem/FileSystemEventDb.cpp
+++ Backend/FileMonitorSubSystem/FileSystemEventDb.cpp
@@ -236,6 +236,7 @@ void FileSystemEventDb::createDb()
     queryCreateTableFolder += " folder_path TEXT NOT NULL,";
     queryCreateTableFolder += " parent_folder_path TEXT,";
     queryCreateTableFolder += " state INTEGER NOT NULL DEFAULT 0,";
+    queryCreateTableFolder += " timestamp TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,";
     queryCreateTableFolder += " PRIMARY KEY(folder_path),";
     queryCreateTableFolder += "	FOREIGN KEY(parent_folder_path) REFERENCES Folder(folder_path) ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED";
     queryCreateTableFolder += ");";
@@ -245,6 +246,7 @@ void FileSystemEventDb::createDb()
     queryCreateTableFile += " file_path TEXT NOT NULL,";
     queryCreateTableFile += " folder_path TEXT,";
     queryCreateTableFile += " state INTEGER NOT NULL DEFAULT 0,";
+    queryCreateTableFile += " timestamp TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,";
     queryCreateTableFile += " PRIMARY KEY(file_path),";
     queryCreateTableFile += " FOREIGN KEY(folder_path) REFERENCES Folder(folder_path) ON DELETE CASCADE ON UPDATE CASCADE";
     queryCreateTableFile += " ); ";
