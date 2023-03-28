5579c537578a5635de70526864dc9ba2807e6d31
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2023-03-28 16:14:40 +0300

    `backupFolderPath` added to `AppConfig`

--- Utility/AppConfig.cpp
+++ Utility/AppConfig.cpp
@@ -21,6 +21,23 @@ AppConfig::~AppConfig()
     delete settings;
 }
 
+bool AppConfig::isDisclaimerAccepted() const
+{
+    QReadLocker readLocker(&lock);
+
+    if(settings->value(KeyDisclaimerAccepted).toString() == "true")
+        return true;
+
+    return false;
+}
+
+void AppConfig::setDisclaimerAccepted(bool newDisclaimerAccepted)
+{
+    QWriteLocker writeLocker(&lock);
+
+   settings->setValue(KeyDisclaimerAccepted, newDisclaimerAccepted);
+}
+
 bool AppConfig::isTrayIconInformed() const
 {
     QReadLocker readLocker(&lock);
@@ -38,19 +55,42 @@ void AppConfig::setTrayIconInformed(bool newTrayIconInformed)
     settings->setValue(KeyTrayIconInformed, newTrayIconInformed);
 }
 
-bool AppConfig::isDisclaimerAccepted() const
+bool AppConfig::isBackupFolderPathValid() const
 {
     QReadLocker readLocker(&lock);
 
-    if(settings->value(KeyDisclaimerAccepted).toString() == "true")
-        return true;
+    QString readValue = settings->value(KeyBackupFolderPath).toString();
 
-    return false;
+    if(readValue.isNull() || readValue.isEmpty())
+        return false;
+
+    if(!QDir(readValue).exists())
+        return false;
+
+    return true;
 }
 
-void AppConfig::setDisclaimerAccepted(bool newDisclaimerAccepted)
+QString AppConfig::getBackupFolderPath() const
+{
+    QReadLocker readLocker(&lock);
+
+    QString readValue = settings->value(KeyBackupFolderPath).toString();
+    readValue = QDir::toNativeSeparators(readValue);
+
+    if(!readValue.endsWith(QDir::separator()))
+        readValue.append(QDir::separator());
+
+    return readValue;
+}
+
+void AppConfig::setBackupFolderPath(const QString &newBackupFolderPath)
 {
     QWriteLocker writeLocker(&lock);
 
-   settings->setValue(KeyDisclaimerAccepted, newDisclaimerAccepted);
+    QString value = QDir::toNativeSeparators(newBackupFolderPath);
+
+    if(!value.endsWith(QDir::separator()))
+        value.append(QDir::separator());
+
+    settings->setValue(KeyBackupFolderPath, value);
 }
--- Utility/AppConfig.h
+++ Utility/AppConfig.h
@@ -16,9 +16,14 @@ public:
     bool isTrayIconInformed() const;
     void setTrayIconInformed(bool newTrayIconInformed);
 
+    bool isBackupFolderPathValid() const;
+    QString getBackupFolderPath() const;
+    void setBackupFolderPath(const QString &newBackupFolderPath);
+
 private:
     static const inline QString KeyDisclaimerAccepted = "disclaimer_accepted";
     static const inline QString KeyTrayIconInformed = "tray_icon_informed";
+    static const inline QString KeyBackupFolderPath = "backup_folder_path";
 
     static QReadWriteLock lock;
 
