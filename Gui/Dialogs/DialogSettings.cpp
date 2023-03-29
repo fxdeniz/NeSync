d0271f83cffa736d7829cd4fb04b80484deb3d00
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2023-03-29 10:17:40 +0300

    make `DatabaseRegistry` get storage folder path from `AppConfig`

--- Gui/Dialogs/DialogSettings.cpp
+++ Gui/Dialogs/DialogSettings.cpp
@@ -1,11 +1,16 @@
 #include "DialogSettings.h"
 #include "ui_DialogSettings.h"
 
+#include "Utility/AppConfig.h"
+
 DialogSettings::DialogSettings(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogSettings)
 {
     ui->setupUi(this);
+
+    AppConfig config;
+    ui->lineEdit->setText(config.getStorageFolderPath());
 }
 
 DialogSettings::~DialogSettings()
