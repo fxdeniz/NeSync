43873b24366fb28aacb7f8ea28a2eb8290ca28e3
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2023-10-13 00:11:03 +0300

    integrate fsm into first endpoint

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,103 +1,88 @@
 #include <QCoreApplication>
+#include <QDir>
+#include <QDebug>
 #include <QJsonObject>
+#include <QJsonDocument>
+#include <QStandardPaths>
 #include <QtHttpServer/QHttpServer>
 #include <QtHttpServer/QHttpServerResponse>
 
-using namespace Qt::StringLiterals;
-
-static inline QString host(const QHttpServerRequest &request)
-{
-    return QString::fromLatin1(request.value("Host"));
-}
+#include "Utility/AppConfig.h"
+#include "FileStorageSubSystem/FileStorageManager.h"
 
 // For routing checkout: https://www.qt.io/blog/2019/02/01/qhttpserver-routing-api
+
+QHttpServerResponse postAddNewFile(const QHttpServerRequest& request)
+{
+    QHttpServerResponse response(QHttpServerResponse::StatusCode::NotImplemented);
+    QByteArray requestBody = request.body();
+
+    if (requestBody.isEmpty())
+    {
+        QString errorMessage = "Body is empty";
+        response = QHttpServerResponse(errorMessage, QHttpServerResponse::StatusCode::BadRequest);
+        return response;
+    }
+
+
+    QJsonParseError jsonError;
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(requestBody, &jsonError);
+
+    if (jsonError.error != QJsonParseError::NoError)
+    {
+        QString errorMessage = "Input format is not parsable json.";
+        response = QHttpServerResponse(errorMessage, QHttpServerResponse::StatusCode::BadRequest);
+        return response;
+    }
+
+    if (!jsonDoc.isObject())
+    {
+        QString errorMessage = "Input json is not an object.";
+        response = QHttpServerResponse(errorMessage, QHttpServerResponse::StatusCode::BadRequest);
+        return response;
+    }
+
+
+    QJsonObject jsonObject = jsonDoc.object();
+
+    QString symbolFolderPath = jsonObject["symbolFolderPath"].toString();
+    QString pathToFile = jsonObject["pathToFile"].toString();
+    QString description = jsonObject["description"].toString();
+    bool isFrozen = jsonObject["isFrozen"].toBool();
+
+    qDebug() << "symbolFolderPath = " << symbolFolderPath;
+    qDebug() << "pathToFile = " << pathToFile;
+    qDebug() << "description = " << description;
+    qDebug() << "isFrozen = " << isFrozen;
+
+    QString storagePath = QStandardPaths::writableLocation(QStandardPaths::StandardLocation::HomeLocation);
+    storagePath = QDir::toNativeSeparators(storagePath) + QDir::separator();
+    storagePath += "nesync_server_";
+    storagePath += QUuid::createUuid().toString(QUuid::StringFormat::WithoutBraces).mid(0, 8);
+    storagePath += QDir::separator();
+
+    QDir().mkpath(storagePath);
+    AppConfig().setStorageFolderPath(storagePath);
+
+    auto fsm = FileStorageManager::instance();
+
+    fsm->addNewFolder(symbolFolderPath, "");
+    fsm->addNewFile(symbolFolderPath, pathToFile, isFrozen, "", description);
+
+    return response;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     QHttpServer httpServer;
-    httpServer.route("/", []() {
-        return "Hello world";
+
+    httpServer.route("/addNewFile", QHttpServerRequest::Method::Post, [](const QHttpServerRequest &request) {
+        return postAddNewFile(request);
     });
 
-    httpServer.route("/query", [] (const QHttpServerRequest &request) {
-        return host(request) + u"/query/"_s;
-    });
-
-    httpServer.route("/query/", [] (qint32 id, const QHttpServerRequest &request) {
-        return u"%1/query/%2"_s.arg(host(request)).arg(id);
-    });
-
-    httpServer.route("/query/<arg>/log", [] (qint32 id, const QHttpServerRequest &request) {
-        return u"%1/query/%2/log"_s.arg(host(request)).arg(id);
-    });
-
-    httpServer.route("/query/<arg>/log/", [] (qint32 id, float threshold,
-                                             const QHttpServerRequest &request) {
-        return u"%1/query/%2/log/%3"_s.arg(host(request)).arg(id).arg(threshold);
-    });
-
-    httpServer.route("/user/", [] (const qint32 id) {
-        return u"User "_s + QString::number(id);
-    });
-
-    httpServer.route("/user/<arg>/detail", [] (const qint32 id) {
-        return u"User %1 detail"_s.arg(id);
-    });
-
-    httpServer.route("/user/<arg>/detail/", [] (const qint32 id, const qint32 year) {
-        return u"User %1 detail year - %2"_s.arg(id).arg(year);
-    });
-
-    httpServer.route("/json/", [] {
-        return QJsonObject{
-            {
-                {"key1", "1"},
-                {"key2", "2"},
-                {"key3", "3"}
-            }
-        };
-    });
-
-    httpServer.route("/assets/<arg>", [] (const QUrl &url) {
-        return QHttpServerResponse::fromFile(u":/assets/"_s + url.path());
-    });
-
-    httpServer.route("/remote_address", [](const QHttpServerRequest &request) {
-        return request.remoteAddress().toString();
-    });
-
-    // Basic authentication example (RFC 7617)
-    httpServer.route("/auth", [](const QHttpServerRequest &request) {
-        auto auth = request.value("authorization").simplified();
-
-        if (auth.size() > 6 && auth.first(6).toLower() == "basic ") {
-            auto token = auth.sliced(6);
-            auto userPass = QByteArray::fromBase64(token);
-
-            if (auto colon = userPass.indexOf(':'); colon > 0) {
-                auto userId = userPass.first(colon);
-                auto password = userPass.sliced(colon + 1);
-
-                if (userId == "Aladdin" && password == "open sesame")
-                    return QHttpServerResponse("text/plain", "Success\n");
-            }
-        }
-        QHttpServerResponse response("text/plain", "Authentication required\n",
-                                     QHttpServerResponse::StatusCode::Unauthorized);
-        response.setHeader("WWW-Authenticate", R"(Basic realm="Simple example", charset="UTF-8")");
-        return response;
-    });
-
-    //! [Using afterRequest()]
-    httpServer.afterRequest([](QHttpServerResponse &&resp) {
-        resp.setHeader("Server", "Qt HTTP Server");
-        return std::move(resp);
-    });
-    //! [Using afterRequest()]
-
-    const auto port = httpServer.listen(QHostAddress::SpecialAddress::Any);
+    qulonglong port = httpServer.listen(QHostAddress::SpecialAddress::Any);
     if (port)
         qDebug() << "running on = " << "localhost:" + QString::number(port);
     else
