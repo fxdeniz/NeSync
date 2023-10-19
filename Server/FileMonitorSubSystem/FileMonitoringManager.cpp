b940b9b6d6a998c7d28df21f382c62737cd3dfba
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2023-10-19 22:09:52 +0300

    add `FileMonitoringManager::slotOnModificationEventDetected()`

--- Server/FileMonitorSubSystem/FileMonitoringManager.cpp
+++ Server/FileMonitorSubSystem/FileMonitoringManager.cpp
@@ -1,5 +1,8 @@
 #include "FileMonitoringManager.h"
+#include "Utility/JsonDtoFormat.h"
+#include "FileStorageSubSystem/FileStorageManager.h"
 
+#include <QDir>
 #include <QFileInfo>
 
 FileMonitoringManager::FileMonitoringManager(FileSystemEventDb *fsEventDb, QObject *parent)
@@ -71,7 +74,33 @@ void FileMonitoringManager::slotOnDeleteEventDetected(const QString &fileName, c
 
 void FileMonitoringManager::slotOnModificationEventDetected(const QString &fileName, const QString &dir)
 {
+    qDebug() << "updateEvent = " << dir << fileName;
+    qDebug() << "";
 
+    bool isFileMonitored = eventDb->isMonitoredFileExist(dir, fileName);
+
+    if(isFileMonitored)
+    {
+        QString currentPath = QDir::toNativeSeparators(dir + fileName);
+
+        auto fsm = FileStorageManager::instance();
+
+        QJsonObject fileJson = fsm->getFileJsonByUserPath(currentPath);
+        QString symbolFilePath = fileJson[JsonKeys::File::SymbolFilePath].toString();
+        qlonglong maxVersionNumber = fileJson[JsonKeys::File::MaxVersionNumber].toInteger();
+
+        QJsonObject versionJson = fsm->getFileVersionJson(symbolFilePath, maxVersionNumber);
+        QString strLastModifiedTimestamp = versionJson[JsonKeys::FileVersion::LastModifiedTimestamp].toString();
+        QDateTime lastModifiedTimestamp = QDateTime::fromString(strLastModifiedTimestamp, Qt::DateFormat::ISODateWithMs);
+        QDateTime currentTimestamp = QFileInfo(currentPath).lastModified();
+
+        bool isFilePersists = fileJson[JsonKeys::IsExist].toBool();
+        bool isFileFrozen = fileJson[JsonKeys::File::IsFrozen].toBool();
+        bool isFileTouched = (lastModifiedTimestamp != currentTimestamp);
+
+        if(isFilePersists && !isFileFrozen && isFileTouched)
+            eventDb->setStatusOfMonitoredFile(dir, fileName, FileSystemEventDb::ItemStatus::Updated);
+    }
 }
 
 void FileMonitoringManager::slotOnMoveEventDetected(const QString &fileName, const QString &oldFileName, const QString &dir)
