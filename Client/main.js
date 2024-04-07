dac2ba7c94c093ba9390f63a830a514cfa902d50
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2024-04-07 10:16:56 +0300

    add missing `;` in `main.js`

--- Client/main.js
+++ Client/main.js
@@ -38,7 +38,7 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
 });
 
