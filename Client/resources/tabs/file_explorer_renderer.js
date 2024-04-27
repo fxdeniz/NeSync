4d641a7d707410f22139294dd5f6163533a4b273
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2024-04-27 16:56:34 +0300

    move 87b2d71 into seperate function

--- Client/resources/tabs/file_explorer_renderer.js
+++ Client/resources/tabs/file_explorer_renderer.js
@@ -1,37 +1,3 @@
-async function postJSON(targetUrl, requestBody) {
-    try {
-      const response = await fetch(targetUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-      });
-  
-      const result = await response.text();
-      console.log("Success:", result);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  async function fetchJSON(targetUrl) {
-    try {
-      const response = await fetch(targetUrl);
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      
-      const result = await response.json();
-      
-      return result;
-
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-    }
-  }
-
 document.addEventListener("DOMContentLoaded", async (event) => {
 
     let inputCurrentPath = document.getElementById('input-current-path');
@@ -41,46 +7,95 @@ document.addEventListener("DOMContentLoaded", async (event) => {
 
     buttonPrev.disabled = true;
     buttonNext.disabled = true;
-    inputCurrentPath.value = '/';
     inputCurrentPath.readOnly = true;
 
-    inputCurrentPath.addEventListener('directoryNavigation', (event) => {
-       inputCurrentPath.value = event.detail.targetPath;
-    });
-
+    inputCurrentPath.addEventListener('directoryNavigation', onDirectoryChangeHandler_inputCurrentPath);
     buttonAddNewFolder.addEventListener('click', onClickHandler_buttonAddNewFolder);
 
-    let folderJson = await fetchJSON(`http://localhost:1234/getFolderContent?symbolPath=${inputCurrentPath.value}`);
-
-    folderJson['childFolders'].forEach(currentFolder => {
-      let tableExplorerBody = document.querySelector('#table-explorer tbody');
-
-      let row = document.createElement('tr');
-      let colName = document.createElement('td');
-      let colLocation = document.createElement('td');
-      
-      colName.innerText = currentFolder['suffixPath'];
-      colLocation.innerText = currentFolder['userFolderPath'];
-
-      row.appendChild(colName);
-      row.appendChild(colLocation);
-      row.dataset.type = "folder";
-      row.dataset.symbolPath = currentFolder['symbolFolderPath'];
-
-      row.addEventListener('click', () => {
-        if(row.dataset.type === "folder") {
-          let navigationEvnet = new CustomEvent('directoryNavigation', 
-                                                {bubbles: true, 
-                                                detail: {'targetPath': row.dataset.symbolPath}});
-          inputCurrentPath.dispatchEvent(navigationEvnet);
-        }
-      });
-
-      tableExplorerBody.appendChild(row);
-    });
-
+    let navigationEvnet = createDirectoryChangeEvent('/');
+    inputCurrentPath.dispatchEvent(navigationEvnet);
   });
+
+async function onClickHandler_buttonAddNewFolder() {
+  const selectedFolderTree = await window.fileExplorerApi.showFolderSelectDialog();
+
+  if(selectedFolderTree) {
+      let stack = [selectedFolderTree];
+      let fileList = [];
+
+      while (stack.length > 0) {
+          let currentFolder = stack.pop();
+          let pathTokens = await window.pathApi.splitPath(currentFolder.folderPath);
+          let symbolFolderSuffix = pathTokens.pop() + "/";
+
+          if(currentFolder.symbolFolderPath === undefined) // Check symbol folder path of the root.
+            currentFolder.symbolFolderPath = "/" + symbolFolderSuffix;
+          else
+            currentFolder.symbolFolderPath += symbolFolderSuffix;
+
+          await sendAddFolderRequest(currentFolder.symbolFolderPath, currentFolder.folderPath);
+
+          for(filePath of currentFolder.childFiles) {
+            let fileName = await window.pathApi.fileNameWithExtension(filePath);
+            fileList.push({symbolFolderPath: currentFolder.symbolFolderPath,
+                            pathToFile: filePath,
+                            description: `First version of <b>${fileName}</b>.`,
+                            isFrozen: false
+            });
+          }
   
+          for (let index = (currentFolder.childFolders.length - 1); index >= 0; index--) {
+              currentFolder.childFolders[index].symbolFolderPath = currentFolder.symbolFolderPath;
+              stack.push(currentFolder.childFolders[index]);
+          }
+      }
+
+      for(currentFile of fileList) {
+        await sendAddFileRequest(currentFile.symbolFolderPath,
+                                  currentFile.pathToFile,
+                                  currentFile.description,
+                                  currentFile.isFrozen);
+      }
+  }
+}
+
+async function onDirectoryChangeHandler_inputCurrentPath(event) {
+  event.target.value = event.detail.targetPath; // Set inputCurrentPath's new value.
+
+  let tableExplorerBody = document.querySelector('#table-explorer tbody');
+  tableExplorerBody.innerHTML = "";  // Clean previous rows from table.
+
+  let folderJson = await fetchJSON(`http://localhost:1234/getFolderContent?symbolPath=${event.detail.targetPath}`);
+
+  if(folderJson.childFolders) {
+    folderJson['childFolders'].forEach(currentFolder => {
+        let row = document.createElement('tr');
+        let colName = document.createElement('td');
+        let colLocation = document.createElement('td');
+        
+        colName.innerText = currentFolder['suffixPath'];
+        colLocation.innerText = currentFolder['userFolderPath'];
+    
+        row.appendChild(colName);
+        row.appendChild(colLocation);
+        row.dataset.type = "folder";
+        row.dataset.symbolPath = currentFolder['symbolFolderPath'];
+    
+        row.addEventListener('click', () => {
+          if(row.dataset.type === "folder") {
+            let navigationEvnet = createDirectoryChangeEvent(row.dataset.symbolPath);
+            event.target.dispatchEvent(navigationEvnet);
+          }
+        });
+    
+        tableExplorerBody.appendChild(row);
+    });
+  }
+}
+
+function createDirectoryChangeEvent(targetPath) {
+  return new CustomEvent('directoryNavigation', {bubbles: true, detail: {'targetPath': targetPath}});
+}
 
 async function sendAddFolderRequest(symbolFolderPath, userFolderPath) {
   let requestBody = {"symbolFolderPath": null, "userFolderPath": null};
@@ -100,46 +115,36 @@ async function sendAddFileRequest(symbolFolderPath, pathToFile, description, isF
   await postJSON('http://localhost:1234/addNewFile', requestBody);    
 }
 
-async function onClickHandler_buttonAddNewFolder() {
-    const selectedFolderTree = await window.fileExplorerApi.showFolderSelectDialog();
+async function postJSON(targetUrl, requestBody) {
+  try {
+    const response = await fetch(targetUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
 
-    if(selectedFolderTree) {
+    const result = await response.text();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
-        let stack = [selectedFolderTree];
-        let fileList = [];
-
-        while (stack.length > 0) {
-            let currentFolder = stack.pop();
-            let pathTokens = await window.pathApi.splitPath(currentFolder.folderPath);
-            let symbolFolderSuffix = pathTokens.pop() + "/";
-
-            if(currentFolder.symbolFolderPath === undefined) // Check symbol folder path of the root.
-              currentFolder.symbolFolderPath = "/" + symbolFolderSuffix;
-            else
-              currentFolder.symbolFolderPath += symbolFolderSuffix;
-
-            await sendAddFolderRequest(currentFolder.symbolFolderPath, currentFolder.folderPath);
-
-            for(filePath of currentFolder.childFiles) {
-              let fileName = await window.pathApi.fileNameWithExtension(filePath);
-              fileList.push({symbolFolderPath: currentFolder.symbolFolderPath,
-                              pathToFile: filePath,
-                              description: `First version of <b>${fileName}</b>.`,
-                              isFrozen: false
-              });
-            }
+async function fetchJSON(targetUrl) {
+  try {
+    const response = await fetch(targetUrl);
     
-            for (let index = (currentFolder.childFolders.length - 1); index >= 0; index--) {
-                currentFolder.childFolders[index].symbolFolderPath = currentFolder.symbolFolderPath;
-                stack.push(currentFolder.childFolders[index]);
-            }
-        }
-
-        for(currentFile of fileList) {
-          await sendAddFileRequest(currentFile.symbolFolderPath,
-                                   currentFile.pathToFile,
-                                   currentFile.description,
-                                   currentFile.isFrozen);
-        }
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    
+    const result = await response.json();
+    
+    return result;
+
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 }
