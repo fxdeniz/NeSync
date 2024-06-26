b038d306268b7ac821f5fe72ce9cdbdbb71a09ed
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2024-06-26 14:09:08 +0300

    init display tree building for file monitor in server poc

--- Client/resources/tabs/file_monitor_renderer.js
+++ Client/resources/tabs/file_monitor_renderer.js
@@ -1,4 +1,60 @@
 document.addEventListener("DOMContentLoaded", async (event) => {
+  let buttonRefresh = document.getElementById(`button-refresh`);
+
+  buttonRefresh.addEventListener(`click`, async (event) => {
+    let newAddedJson = await fetchJSON(`http://localhost:1234/newAddedList`);
+    let deletedJson = await fetchJSON(`http://localhost:1234/deletedList`);
+    let updatedJson = await fetchJSON(`http://localhost:1234/updatedFileList`);
+  
+    console.log(`newAdded: ${JSON.stringify(newAddedJson, null, 2)}`);
+    console.log(`deleted: ${JSON.stringify(deletedJson, null, 2)}`);
+    console.log(`updated: ${JSON.stringify(updatedJson, null, 2)}`);
+
+    let folderPathSet = new Set();
+
+    for(currentFolder of newAddedJson.folders) {
+      folderPathSet.add(currentFolder);
+    }
+
+    for(currentFolder in newAddedJson.files) {
+      folderPathSet.add(currentFolder);
+    }
+
+    for(currentFolder of deletedJson.folders) {
+      folderPathSet.add(currentFolder);
+    }
+
+    for(currentFolder in deletedJson.files) {
+      folderPathSet.add(currentFolder);
+    }
+
+    Object.keys(updatedJson).forEach(key => {
+      folderPathSet.add(key);
+    });
+
+    let folderPathList = Array.from(folderPathSet);
+    folderPathList.sort((a, b) => a.length - b.length);
+
+    let tree = {};
+
+    for(path of folderPathList) {
+      tree[path] = [];
+
+      if(path in newAddedJson.files)
+        tree[path].push(...newAddedJson.files[path]);
+
+      if(path in deletedJson.files)
+        tree[path].push(...deletedJson.files[path]);
+
+      if(path in updatedJson)
+        tree[path].push(...updatedJson[path]);
+
+    }
+
+    console.log(`tree: ${JSON.stringify(tree, null, 2)}`);
+
+
+  });
 
 });
 
