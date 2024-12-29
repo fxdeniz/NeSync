706c1cd25ed7a69a1aa509ba45d295bcabec4e98
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2024-12-29 22:06:18 +0300

    update `__dirname` doc in `main.js`

--- Client/main.js
+++ Client/main.js
@@ -6,6 +6,7 @@ import CacheApi from './CacheApi.mjs';
 
 // https://iamwebwiz.medium.com/how-to-fix-dirname-is-not-defined-in-es-module-scope-34d94a86694d
 // https://byby.dev/node-dirname-not-defined
+// https://nodejs.org/api/esm.html#importmeta
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
