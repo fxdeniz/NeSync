1ec47bf4c4e25ebfd60f930002db3d20ef0f43f3
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2025-01-04 19:38:06 +0300

    add displaying file name on load in `file_info.html`

--- Client/gui/pages/file_info_renderer.js
+++ Client/gui/pages/file_info_renderer.js
@@ -2,5 +2,13 @@ import FolderApi from "../rest_api/FolderApi.mjs";
 import FileApi from "../rest_api/FileApi.mjs"
 
 document.addEventListener("DOMContentLoaded", async (event) => {
-    console.log(`target file = ${await window.appState.get("currentFile")}`);
+    const symbolPath = await window.appState.get("currentFile");
+
+    const fileApi = new FileApi("localhost", 1234);
+    const info = await fileApi.get(symbolPath)
+
+    const divFileName = document.getElementById("div-file-name");
+    const inputCurrentPath = document.getElementById("input-current-path");
+    divFileName.textContent = info.fileName;
+    inputCurrentPath.value = info.symbolFilePath;
 });
