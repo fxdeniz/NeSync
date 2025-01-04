db8d31d8cf2c2c85bdc115dd7642ca631cc325ea
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2025-01-04 20:23:58 +0300

    init version listing in `file_info.html`

--- Client/gui/pages/file_info_renderer.js
+++ Client/gui/pages/file_info_renderer.js
@@ -11,4 +11,32 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     const inputCurrentPath = document.getElementById("input-current-path");
     divFileName.textContent = info.fileName;
     inputCurrentPath.value = info.symbolFilePath;
+
+    const olVersions = document.getElementById("ol-versions");
+    info.versionList.forEach(item => olVersions.appendChild(createListItem(item.versionNumber, item.description)));
 });
+
+function createListItem(versionNumber, description) {
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-start clickable";
+    li.setAttribute("role", "button");
+  
+    const textDiv = document.createElement("div");
+    textDiv.className = "ms-2 me-auto";
+
+    const subheadingDiv = document.createElement('div');
+    subheadingDiv.className = "fw-bold";
+    subheadingDiv.textContent = "Subheading";
+
+    textDiv.appendChild(subheadingDiv);
+    textDiv.appendChild(document.createTextNode(description));
+
+    const badge = document.createElement("span");
+    badge.className = "badge text-bg-primary rounded-pill";
+    badge.textContent = versionNumber;
+
+    li.appendChild(textDiv);
+    li.appendChild(badge);
+
+    return li;
+}
