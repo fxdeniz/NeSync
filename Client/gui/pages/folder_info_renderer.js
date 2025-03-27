10412923bc957b0d4794f37165fddfc3c50c0d2b
fxdeniz <104620840+fxdeniz@users.noreply.github.com>
2025-03-27 19:33:08 +0300

    make back button functional in folder info

--- Client/gui/pages/folder_info_renderer.js
+++ Client/gui/pages/folder_info_renderer.js
@@ -3,10 +3,19 @@ import FileApi from "../rest_api/FileApi.mjs"
 import VersionApi from "../rest_api/VersionApi.mjs"
 
 document.addEventListener("DOMContentLoaded", async (event) => {
-    const folderInfo = await window.appState.get("currentFolder");
+    const folderApi = new FolderApi("localhost", 1234);
+    const currentFolder = await window.appState.get("currentFolder");
+    const parentFolder = await folderApi.get(currentFolder.parentFolderPath);
 
     const divFolderName = document.getElementById("div-folder-name");
     const inputCurrentPath = document.getElementById("input-current-path");
-    divFolderName.textContent = folderInfo.suffixPath.slice(0, -1); // Remove last '/'.
-    inputCurrentPath.value = folderInfo.symbolFolderPath;
+    const buttonBack = document.getElementById("button-back");
+
+    divFolderName.textContent = currentFolder.suffixPath.slice(0, -1); // Remove last '/'.
+    inputCurrentPath.value = currentFolder.symbolFolderPath;
+
+    buttonBack.addEventListener('click', async clickEvent => {
+        await window.appState.set("currentFolder", parentFolder);
+        window.router.routeToFileExplorer();
+    });
 });
