83f9e4809e8c28628f6df603a7e8d29da9429b71
Deniz Yilmazok <104620840+fxdeniz@users.noreply.github.com>
2025-04-08 14:01:35 +0300

    refactor save changes page to use port from `appState`

--- Client/gui/pages/save_changes_renderer.js
+++ Client/gui/pages/save_changes_renderer.js
@@ -3,10 +3,10 @@ import FileApi from "../rest_api/FileApi.mjs"
 import MonitorApi from "../rest_api/MonitorApi.mjs";
 
 document.addEventListener("DOMContentLoaded", async (event) => {
-
-    let folderApi = new FolderApi('localhost', 1234);
-    let fileApi = new FileApi('localhost', 1234);
-    let monitorApi = new MonitorApi('localhost', 1234);
+    const serverPort = await window.appState.get("serverPort");
+    let folderApi = new FolderApi('localhost', serverPort);
+    let fileApi = new FileApi('localhost', serverPort);
+    let monitorApi = new MonitorApi('localhost', serverPort);
 
     let newAddedJson = await monitorApi.getNewAddedList();
     let deletedJson = await monitorApi.getDeletedList();
